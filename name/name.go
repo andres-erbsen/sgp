b6e5116adf5242e3839817ffea00cd2d6c5646e5
Andres Erbsen <andres@krutt.org>
2013-11-14 23:35:07 +0200

    Simple self-certification generator

---
+++ name/name.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"os"
+	"io/ioutil"
+	"log"
+	"code.google.com/p/goprotobuf/proto"
+	"github.com/andres-erbsen/sgp"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("USAGE: ", os.Args[0], " NAME < secretkey")
+	}
+	sk_bytes, err := ioutil.ReadAll(os.Stdin)
+	var sk sgp.SecretKey
+	err = sk.Parse(sk_bytes)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	attribution := &sgp.Attribution{}
+	attribution.Name = []byte(os.Args[1])
+	attribution.Pubkey = sk.Entity.Bytes
+
+	atb_bytes, err := proto.Marshal(attribution)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	cert := sk.Sign(atb_bytes)
+	log.Print(atb_bytes, sk, sk.Entity)
+	if ! sk.Entity.Verify(cert) {
+		log.Fatal("Signature verification failed")
+	}
+	cert_bytes, err := proto.Marshal(cert)
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout.Write(cert_bytes)
+}
+
